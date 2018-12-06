6466ae2a1803b7ab32a9dea10f030f545e19044e
Garion's Blade <yuxuanng@hotmail.com>
2018-12-06 17:39:56 -0600

    Preprocessed data

---
+++ main.py
@@ -0,0 +1,27 @@
+from train_dataset_processing import *
+from datetime_processing import *
+from action_processing import *
+import pandas as pd
+
+df = pd.read_csv('train_and_count.csv')
+df = simplify_classes(df)
+print("simplified classes")
+df = process_age(df)
+print("processed age")
+df = process_affiliate_channel(df)
+print("processed channel")
+df = process_languages(df)
+print("processed languages")
+df = process_first_browser(df)
+print("processed browser")
+df = diff_active_create(df)
+print("processed active diff")
+df = process_date(df)
+print("processed date")
+cols = df.columns.tolist()
+new_cols = cols[:-3] + cols [-2:] + cols[-3:-2]
+df = df[new_cols]
+print("formatted date")
+df = processAction(df)
+print("processed action")
+df.to_csv('processed_user.csv', index=False)
