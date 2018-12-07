09336eab50364669c0da4c1b8b075ca07c3f2e8e
rhowardliu <rhowardliu@gmail.com>
2018-12-06 23:15:20 -0600

    commit for pull

--- adding_sessions_count.py
+++ adding_sessions_count.py
@@ -1,20 +1,9 @@
 import pandas as pd
 
-# get count of unique ids seen in sessions.csv
-sessions = pd.read_csv('downloaded_datasets/sessions.csv')
-counted = sessions['user_id'].value_counts(sort=False)
-counted.to_csv('sessions_counted.csv')
+def addSessionsCount(df_t, df_s):
+  counted = df_s['user_id'].value_counts(sort=False)
+  new = df_t.set_index('id').join(counted).rename(columns={'user_id':'session_count'})
+  return new
 
-# I manually added a row to sessions_counted with col names: [id,session_count]
-
-# join the train and session count data
-main = pd.read_csv('downloaded_datasets/train_users_2.csv')
-new = main.set_index('id').join(counted.set_index('id'))
-
-# swap columns around
-cols = new.columns.tolist()
-cols = cols[:-2] + cols[-1:] + cols[-2:-1]
-new = new[cols]
-
-# save to file
-new.to_csv('train_and_count.csv') 
+if __name__ == '__main__':
+  addSessionsCount(pd.read_csv('downloaded_datasets/train_users_2.csv'), pd.read_csv('downloaded_datasets/sessions.csv'))
