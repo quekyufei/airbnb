443e628436bbf35406db9b7a11c2ad74cce80aff
Howard <rhowardliu@gmail.com>
2018-12-06 18:40:43 -0600

    Edited sessions count file to look nicer

--- adding_sessions_count.py
+++ adding_sessions_count.py
@@ -1,20 +1,15 @@
 import pandas as pd
 
 # get count of unique ids seen in sessions.csv
-sessions = pd.read_csv('/Users/QuekYufei/Downloads/all/sessions.csv')
-counted = sessions['user_id'].value_counts(sort=False)
-counted.to_csv('/Users/QuekYufei/Downloads/all/sessions_counted.csv')
+# sessions = pd.read_csv('/Users/QuekYufei/Downloads/all/sessions.csv')
+# counted = sessions['user_id'].value_counts(sort=False)
+# counted.to_csv('/Users/QuekYufei/Downloads/all/sessions_counted.csv')
 
-# I manually added a row to sessions_counted with col names: [id,session_count]
 
-# join the train and session count data
-main = pd.read_csv('/Users/QuekYufei/Downloads/all/train_users_2.csv')
-new = main.set_index('id').join(counted.set_index('id'))
+def addSessionsCount(df_t, df_s):
+  counted = df_s['user_id'].value_counts(sort=False)
+  new = df_t.set_index('id').join(counted).rename(columns={'user_id':'session_count'})
+  return new
 
-# swap columns around
-cols = new.columns.tolist()
-cols = cols[:-2] + cols[-1:] + cols[-2:-1]
-new = new[cols]
-
-# save to file
-new.to_csv('/Users/QuekYufei/Downloads/all/train_and_count.csv')
+if __name__ == '__main__':
+  addSessionsCount(pd.read_csv('downloaded_datasets/train_users_2.csv'), pd.read_csv('downloaded_datasets/sessions.csv'))
